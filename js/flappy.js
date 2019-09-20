819083342f334d4931450b162e83ba6eae4ee8ab
Daniel H. Fuzari <dhfuzari@gmail.com>
2019-09-19 22:04:41 -0300

    feat(flappy.js/flappy.html): function ParOfPipes implemented to return a par of pipes element

--- js/flappy.js
+++ js/flappy.js
@@ -5,28 +5,54 @@
  * @param {string} className 
  * @return {Object} elem
  */
-
 function newElement(tagName, className) {
-    const elem = document.creacteElement(tagName);
+    const elem = document.createElement(tagName);
     elem.className = className;
     return elem;
 }
 
 /** @function
- * @name constructPipe
- * @description A factory function wich creates a new pipe element
+ * @name ConstructPipe
+ * @description A constructor function wich creates a new pipe element
  * @param {boolean} reverse 
  * @return {null} null
  */
-function constructPipe(reverse = false) {
+function ConstructPipe(reverse = false) {
     this.element = newElement('div', 'pipe');
 
-    const border = newElement('div', 'border');
-    const body = newElement('div', 'body');
+    const borderElement = newElement('div', 'border');
+    const bodyElement = newElement('div', 'body');
 
     // Append border and body in pipe element accordind reverse value
-    this.element.appendChild(reverse ? body : border);
-    this.element.appendChild(reverse ? border : body);
+    this.element.appendChild(reverse ? bodyElement : borderElement);
+    this.element.appendChild(reverse ? borderElement : bodyElement);
 
-    this.setHeight = height => body.style.height = `${height}px`;
+    this.setHeight = height => bodyElement.style.height = `${height}px`;
 }
+
+function parOfPipes(height, slot, xAxis) {
+    this.element = newElement('div', 'par-of-pipes');
+
+    this.superior = new ConstructPipe(true);
+    this.inferior = new ConstructPipe(false);
+
+    this.element.appendChild(this.superior.element);
+    this.element.appendChild(this.inferior.element);
+
+    this.raffleSlot = () => {
+        const superiorHeight = Math.random() * (height - slot);
+        const inferiorHeight = height - slot - superiorHeight;
+        this.superior.setHeight(superiorHeight);
+        this.inferior.setHeight(inferiorHeight);
+    }
+
+    this.getX = () => parseInt(this.element.style.left.split('px')[0]);
+    this.setX = x => this.element.style.left = `${x}px`;
+    this.getWidth = () => this.element.clientWidth;
+
+    this.raffleSlot();
+    this.setX(xAxis);
+}
+
+const pipe1 = new parOfPipes(700, 200, 800);
+document.querySelector('[wm-flappy]').appendChild(pipe1.element);
